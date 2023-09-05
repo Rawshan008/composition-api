91a1d408769e4b13a885ad98a61fb9742dfd8120
Rawshan Ali <pust.love008@gmail.com>
2023-09-05 13:24:49 +0600

    compute porperty done

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,38 @@
 <template>
-  <h1>Hello </h1>
+  <h1>Employee Present: {{ employeePresent }}</h1>
+  <h2>Full Name: {{ fullName }}</h2>
+
 </template>
+
+<script setup>
+import { computed, reactive } from 'vue';
+
+  const employee = reactive({
+    books: [
+      'book 1',
+      'book 2',
+      'book 3'
+    ]
+  });
+
+  const empoyeeName = reactive({
+    firstName: 'Rawshan',
+    lastName: 'Ali'
+  });
+
+  const employeePresent = computed(() => {
+    return employee.books.length > 0 ? 'Yes' : 'No';
+  });
+
+  const fullName = computed({
+    get(){
+      return empoyeeName.firstName+ ' ' + empoyeeName.lastName;
+    },
+
+    set(newName) {
+      [empoyeeName.firstName, empoyeeName.lastName] = newName.split(' ');
+    }
+  })
+
+
+</script>
