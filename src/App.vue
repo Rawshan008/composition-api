ac2d0d549d636d4b4f7b2cd8bb8fc08ed76d1831
Rawshan Ali <pust.love008@gmail.com>
2023-09-05 13:00:11 +0600

    Reactive vue object

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Hello </h1>
+  <h1>{{ heading }} </h1>
+  <div>
+    <input v-model="heading" type="text">
+  </div>
+
+  <div>
+    <h1>{{ counterObj.title }}</h1>
+    <button @click="handleDecrease">Desrease</button>
+    <h1>{{ counterObj.count }}</h1>
+    <button @click="handleIncrease">Increase</button>
+  </div>
 </template>
+
+<script setup>
+import { reactive, ref } from 'vue';
+
+  const heading = ref('This is title');
+
+  const counterObj = reactive({
+    count: 0,
+    title: "Counter Title "
+  });
+
+  const handleDecrease = ()  => {
+    counterObj.count--;
+  }
+
+  const handleIncrease = () => {
+    counterObj.count++;
+  }
+</script>
